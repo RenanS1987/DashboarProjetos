b70a8b99aea3d0916a0c8bbbc04535388d5c8205
Renan Silva <silva.renan1987@gmail.com>
2025-11-12 21:17:21 -0300

    Corrigir URL do Vercel no CORS

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://dashboar-projetos.vercel.app",
+        "https://dashboarprojetos.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
